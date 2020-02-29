93c223d28f5c5bcff6580b2c8cc66cd2d713201e
Trick <petropatrickj@gmail.com>
2020-02-29 13:48:25 -0500

    adding JawsDB add on

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-//const password = require("../password")
+password = require("../password")
+var connection = {}
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgersDB"
+console.log(process.env.JAWSDB_URL)
+if(process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+   connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: password,
+    database: "burgersDB"
+
 });
+
+}
 
 // Make connection.
 connection.connect(function(err) {
